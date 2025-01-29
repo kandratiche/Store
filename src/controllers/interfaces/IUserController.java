fb9c9974d9affea849fe64d74296214c2d030f73
Sultanzer <sultanfox8@gmail.com>
2025-01-29 21:27:42 +0500

    corrected code

--- src/controllers/interfaces/IUserController.java
+++ src/controllers/interfaces/IUserController.java
@@ -3,9 +3,6 @@ package controllers.interfaces;
 public interface IUserController {
 
     boolean auth(String name, String surname, String password);
+
     String reg(String name, String surname, String password);
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> e24a5b5f689ea966da06eaaf5e02a855f918ca55
