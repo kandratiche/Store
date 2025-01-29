2aed4586b10ff0d245bc66d891181568e2488536
Sultanzer <sultanfox8@gmail.com>
2025-01-29 21:43:17 +0500

    corrected code

--- src/repositories/interfaces/IUserRepository.java
+++ src/repositories/interfaces/IUserRepository.java
@@ -4,10 +4,7 @@ import models.User;
 
 public interface IUserRepository {
     boolean auth(String name, String surname, String password);
+
     boolean reg(User user);
 
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> e24a5b5f689ea966da06eaaf5e02a855f918ca55
